6b0ae0e531a1b7886bf52f3cee4f648431ca54b8
Julio Calderon <juliocalderon238@gmail.com>
2022-08-05 12:46:45 -0400

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Route } from "wouter";
 import "./App.css";
 import Login from "./Components/LoginComponent/Login";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Loader from "./Components/LoadingComponent/Loader";
+import Home from "./Pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={Login} />
-      <Route path="/home" />
+      <Route path="/home" component={Home} />
       <Route path="/loader" component={Loader} />
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,18 @@
 import {useLocation} from 'wouter'
-
+import { Layout } from 'antd'
+import '../App.css'
 
 export default function Home() {
     
-    
-    
-    return (<>
-    
-    </>)
+    const {Header, Footer, Sider, Content} = Layout
+
+    return (
+        <Layout>
+      <Sider width={200} className="Sider" >Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>)
 }
